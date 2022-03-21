160443f7bc45f5cf4528bbbbe9e400b2f8c4ac5a
manuEleven <esolom@eleven-labs.com>
2022-03-22 00:45:32 +0100

    feat(deploy-config): update keys and config through solana CLI

--- programs/blastfm/src/lib.rs
+++ programs/blastfm/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("XNFCVpn6ujGoEEsDJxQWJ36k7HnUQVPzrWc3YBosq9u");
 
 #[program]
 pub mod myepicproject {
