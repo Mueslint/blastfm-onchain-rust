66035aba394059b039323d6b9faaa3466a1b449d
manuEleven <esolom@eleven-labs.com>
2022-04-04 14:51:11 +0200

    add delete, update, reset spotify id

--- programs/blastfm/src/lib.rs
+++ programs/blastfm/src/lib.rs
@@ -4,28 +4,81 @@ use anchor_lang::solana_program::entrypoint::ProgramResult;
 declare_id!("XNFCVpn6ujGoEEsDJxQWJ36k7HnUQVPzrWc3YBosq9u");
 
 #[program]
-pub mod myepicproject {
+pub mod blastfm {
     use super::*;
     pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
-        base_account.total_gifs = 0;
+        base_account.total_musics = 0;
         Ok(())
     }
 
-    // The function now accepts a gif_link param from the user. We also reference the user from the Context
-    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
+    // The function now accepts a music_id param from the user. We also reference the user from the Context
+    pub fn add_music(ctx: Context<AddMusic>, music_id: String) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
         let user = &mut ctx.accounts.user;
 
         // Build the struct.
         let item = ItemStruct {
-            gif_link: gif_link.to_string(),
+            music_id: music_id.to_string(),
             user_address: *user.to_account_info().key,
         };
 
-        // Add it to the gif_list vector.
-        base_account.gif_list.push(item);
-        base_account.total_gifs += 1;
+        // Add it to the music_list vector.
+        base_account.music_list.push(item);
+        base_account.total_musics += 1;
+        Ok(())
+    }
+
+    pub fn delete_music(ctx: Context<AddMusic>, deleted_music_id: String) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+
+        // filter the previous list .
+        let filtered_music_list: Vec<ItemStruct> = base_account
+            .music_list
+            .iter()
+            .filter(|music_item| music_item.music_id == deleted_music_id)
+            .cloned()
+            .collect();
+
+        // replace the precious value with the filtered one
+        base_account.music_list = filtered_music_list;
+        base_account.total_musics -= 1;
+
+        Ok(())
+    }
+
+    pub fn update_music(
+        ctx: Context<AddMusic>,
+        old_music_id: String,
+        new_music_id: String,
+    ) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+
+        let new_music_item = ItemStruct {
+            music_id: new_music_id.to_string(),
+            user_address: *user.to_account_info().key,
+        };
+        // filter the previous list .
+        let mut filtered_music_list: Vec<ItemStruct> = base_account
+            .music_list
+            .iter()
+            .filter(|music_item| music_item.music_id == old_music_id)
+            .cloned()
+            .collect();
+        filtered_music_list.push(new_music_item);
+
+        // replace the precious value with the filtered one
+        base_account.music_list = filtered_music_list;
+
+        Ok(())
+    }
+
+    pub fn reset_all_music(ctx: Context<ResetMusic>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.music_list = Vec::new();
+        base_account.total_musics = 0;
+
         Ok(())
     }
 }
@@ -39,25 +92,31 @@ pub struct StartStuffOff<'info> {
     pub system_program: Program<'info, System>,
 }
 
-// Add the signer who calls the AddGif method to the struct so that we can save it
+// Add the signer who calls the AddMusic method to the struct so that we can save it
 #[derive(Accounts)]
-pub struct AddGif<'info> {
+pub struct AddMusic<'info> {
     #[account(mut)]
     pub base_account: Account<'info, BaseAccount>,
     #[account(mut)]
     pub user: Signer<'info>,
 }
 
+#[derive(Accounts)]
+pub struct ResetMusic<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
 // Create a custom struct for us to work with.
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
 pub struct ItemStruct {
-    pub gif_link: String,
+    pub music_id: String,
     pub user_address: Pubkey,
 }
 
 #[account]
 pub struct BaseAccount {
-    pub total_gifs: u64,
+    pub total_musics: u64,
     // Attach a Vector of type ItemStruct to the account.
-    pub gif_list: Vec<ItemStruct>,
+    pub music_list: Vec<ItemStruct>,
 }
--- tests/blastfm.js
+++ tests/blastfm.js
@@ -7,7 +7,7 @@ const main = async() => {
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
 
-  const program = anchor.workspace.Myepicproject;
+  const program = anchor.workspace.Blastfm;
   const baseAccount = anchor.web3.Keypair.generate();
   let tx = await program.rpc.startStuffOff({
     accounts: {
@@ -20,10 +20,40 @@ const main = async() => {
   console.log("📝 Your transaction signature", tx);
 
   let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  console.log('👀 GIF Count', account.totalGifs.toString())
+  console.log('👀 Music Count', account.totalMusics.toString())
 
-  // You'll need to now pass a GIF link to the function! You'll also need to pass in the user submitting the GIF!
-  await program.rpc.addGif("insert_a_giphy_link_here", {
+  // You'll need to now pass a Music link to the function! You'll also need to pass in the user submitting the Music!
+  console.log('---------------------------------------')
+
+
+  console.log('Adding a new music!')
+  await program.rpc.addMusic("spotify_track_1", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+  console.log('And an extra one ;)')
+
+  await program.rpc.addMusic("spotify_track_2", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+  // Call the account.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 Music Count', account.totalMusics.toString())
+
+  // Access music_list on the account!
+  console.log('👀 Music List', account.musicList)
+
+
+  console.log('---------------------------------------')
+
+
+  console.log('Updating a music')
+  await program.rpc.updateMusic("spotify_track_2", "spotify_track_3", {
     accounts: {
       baseAccount: baseAccount.publicKey,
       user: provider.wallet.publicKey,
@@ -32,10 +62,47 @@ const main = async() => {
   
   // Call the account.
   account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  console.log('👀 GIF Count', account.totalGifs.toString())
+  console.log('👀 Music Count', account.totalMusics.toString())
+    
+  // Access music_list on the account!
+  console.log('👀 Music List', account.musicList)
 
-  // Access gif_list on the account!
-  console.log('👀 GIF List', account.gifList)
+
+  console.log('---------------------------------------')
+
+
+  console.log('Deleting a music')
+  await program.rpc.deleteMusic("spotify_track_1", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+    
+  // Call the account.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 Music Count', account.totalMusics.toString())
+      
+  // Access music_list on the account!
+  console.log('👀 Music List', account.musicList)
+
+
+  console.log('---------------------------------------')
+
+  
+  console.log('Reseting all musics!')
+  await program.rpc.resetAllMusic({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      // user: provider.wallet.publicKey,
+    },
+  });
+    
+  // Call the account.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 Music Count', account.totalMusics.toString())
+
+  // Access music_list on the account!
+  console.log('👀 Music List', account.musicList)
 }
 
 const runMain = async () => {
